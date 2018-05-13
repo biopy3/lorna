081a37428cef0f85a92c5b31a04fbf8e71d060d3
王瑞 <wonray@oumizus-MBP.lan>
2018-05-13 18:44:47 +0800

    2018/05/13

--- alignment/clustalw_tools.py
+++ alignment/clustalw_tools.py
@@ -1,29 +1,26 @@
 import subprocess,os
 
 input_dir = input("Please put in a path of dirctory:").strip().strip('\'')
-print(input_dir)
+os.environ['PATH'] = os.environ['PATH']+':'+os.getcwd()
 def handle_fasta_file(input_dir):
-	li =[]
-	for dirpath,dirnames,filenames in os.walk(input_dir):
-		for filename in filenames:
-			postfix = os.path.splitext(filename)[1]
-			if postfix == '.fasta' or postfix == '.fas' or postfix == '.fa' or postfix == '.txt' or postfix == '.xfasta':
-				li.append(os.path.join(dirpath,filename))
-	return li
+    li =[]
+    filename = []
+    for dirpath,dirnames,filenames in os.walk(input_dir):
+        for filename in filenames:
+            postfix = os.path.splitext(filename)[1]
+            if postfix == '.fasta' or postfix == '.fas' or postfix == '.fa' or postfix == '.txt' or postfix == '.xfasta':
+                file_path = os.path.join(dirpath,filename)
+                filename = os.path.splitext(filename)[0]
+                tuple = file_path,filename
+                li.append(tuple)
+    return li
 
-li = handle_fasta_file(input_dir)
+li= handle_fasta_file(input_dir)
 print(li)
-for input_file in li:
+for i in li:
     try:
-	output_dir = os.path.splitext(input_file)[0]
-	print(input_file)
-	ps = subprocess.Popen(["clustalw",],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-	output_lines = ps.stdout.readlines()
-	f = open(output_dir+"_results/RNAalifold.txt","w")
-	for line in output_lines:
-		f.write(str(line,encoding = "utf-8"))
-	f.close()
-
+        output_dir = os.getcwd()+'/results/'
+        ps = subprocess.call(["clustalw2","-INFILE="+i[0],"-ALIGN","-QUIET","-OUTPUT=FASTA","-OUTFILE="+output_dir+i[1]+'_aligned.fasta'])
     except:
-	print("The file is wrong:"+input_file)
-	continue
+        print("The file is wrong:"+i[0])
+        continue
